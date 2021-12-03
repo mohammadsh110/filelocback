72a47f0ac3f9a1ec4f94042e14c6818f7e2560f1
mohammadsh110 <m.shammasi@gmail.com>
2021-12-02 18:50:16 -0800

    Update urls.py

--- testsite/urls.py
+++ testsite/urls.py
@@ -17,10 +17,9 @@ from django.contrib import admin
 from django.urls import path
 
 
-from testsite.view import uploadImage, location, views
+from testsite.view import uploadImage, location
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('uploadImage/', uploadImage, name='uploadImage'),
     path('location/', location, name='uploadImage'),
