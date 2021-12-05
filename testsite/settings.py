ccdd5e4675db8332d9e870c4ed23105e828cf4ba
mohammadsh110 <m.shammasi@gmail.com>
2021-12-04 22:26:12 -0800

    Update settings.py

--- testsite/settings.py
+++ testsite/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = (
-    'osanalyzer.herokuapp.com:3000',
+    'filesharelocation.herokuapp.com:3000',
 )
 ROOT_URLCONF = 'testsite.urls'
 
